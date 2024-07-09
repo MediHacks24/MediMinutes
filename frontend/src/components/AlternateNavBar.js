963c995f5256c56419e818bbaed79ab32a9cea33
TylerBeach <TylerAllenBeach@gmail.com>
2024-07-08 21:39:36 -0600

    Major UI update

--- frontend/src/components/AlternateNavBar.js
+++ frontend/src/components/AlternateNavBar.js
@@ -4,17 +4,17 @@ import React from 'react'
 export default function Navbar() {
   return (
     <nav className='w-full h-fit fixed top-0 z-[999999]'>
-      <ul className='flex flex-row gap-x-6 text-2xl pt-4 px-8'>
+      <ul className='flex flex-row gap-x-6 text-2xl bg-[#20AC58] pt-4'>
         <li className='flex-1 text-white'>
           <a href='/' className='bg-[#20AC58] px-5 pb-2 rounded-md'>Logo</a>
         </li>
         <div className='flex-[2] flex flex-row gap-x-20 justify-center text-white rounded-md px-5 pb-2'>
-          <Link href='/' className='text-black'>About</Link>
-          <Link href='/' className='text-black'>Features</Link>
-          <Link href='/' className='text-black'>Resources</Link>
+          <Link href='/' className='text-white'>About</Link>
+          <Link href='/' className='text-white'>Features</Link>
+          <Link href='/' className='text-white'>Resources</Link>
         </div>
         <div className='flex-[1] flex flex-row justify-end gap-x-4 text-white rounded-md px-5 pb-2'>
-          <Link href='/' className='text-black'>Log In</Link>
+          <Link href='/' className='text-white'>Log In</Link>
           <Link href='/' className='bg-[#20AC58] text-white rounded-md px-5 pb-2'>Sign Up</Link>
         </div>
       </ul>
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import { json } from 'react-router-dom';
+
+export default function Sidebar({ data, setCurrentSection }) {
+    const [keyArray, setKeyArray] = useState([]);
+
+    useEffect(() => {
+        console.log(data)
+    }, []);
+
+    return (
+        <div className='w-[500px] flex flex-col gap-y-2 calcPageHeight border-r border-black'>
+            <div className='fixed left-0 bg-[#20AC58] w-5 h-[100vh] top-0'></div>
+            <div className='flex flex-col gap-y-20 pl-8 '>
+                <h1 className='text-4xl font-extrabold'>Sections</h1>
+                <ul className='
+            overflow-hidden text-ellipsis whitespace-nowrap flex flex-col gap-y-2 '>
+                    {data.map((key, index) => (
+                        <li key={index} onClick={() => setCurrentSection(key)} className='text-nowrap text-lg font-semibold'>{key.split("-")[1]}</li>
+                    ))}
+                </ul>
+                <button className='bg-[#20AC58] max-w-[200px] text-white p-2 rounded-md'>Take Quiz</button>
+            </div>
+
+        </div>
+    )
+}
