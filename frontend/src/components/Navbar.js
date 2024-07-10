dc8f3ed77a430dc0b7b56c78b910199fd176067d
TylerBeach <TylerAllenBeach@gmail.com>
2024-07-10 15:10:45 -0600

    update navbar + navigation + url routing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,42 @@
 import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const scrollToSection = (section) => {
+    document.getElementById(section).scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <nav className='w-[100vw] h-fit fixed top-0 z-[100] bg-white border-b border-black'>
       <div className='items-center flex flex-row gap-x-6 text-2xl px-8 py-2'>
         <div className='flex-1 text-white h-full'>
+        {router.pathname === '/' ? (
+          <a onClick={() => scrollToSection('Home')} className='bg-[#20AC58] px-5 rounded-md py-2'>Logo</a>
+        ) : (
           <Link href='/' className='bg-[#20AC58] px-5 rounded-md py-2'>Logo</Link>
-        </div>
+        ) }
+          </div>
         <div className='flex-[2] flex flex-row gap-x-20 h-full text-white rounded-md px-5 justify-center'>
-          <Link href='/about' className='text-black'>About</Link>
-          <Link href='/features' className='text-black'>Features</Link>
-          <Link href='/categories' className='text-black'>Categories</Link>
+          {router.pathname === '/' ? (
+            <div className='flex-[2] flex flex-row gap-x-20 h-full text-white rounded-md px-5 justify-center'>
+              <a onClick={() => scrollToSection('About')} className='text-black cursor-pointer'>About</a>
+              <a onClick={() => scrollToSection('Features')} className='text-black cursor-pointer'>Features</a>
+              <Link href='/categories' className='text-black'>Categories</Link>
+            </div>
+          ) : (
+            <div className='flex-[2] flex flex-row gap-x-20 h-full text-white rounded-md px-5 justify-center'>
+            <Link href="/?section=About" className='text-black'>About</Link>
+            <Link href="/?section=Features" className='text-black'>Features</Link>
+              <Link href='/categories' className='text-black'>Categories</Link>
+            </div>
+          )}
         </div>
         <div className='flex-[1] flex flex-row h-full gap-x-4 content-center text-white rounded-md px-5 justify-end'>
           <Link href='/account' className='text-black py-2'>Log In</Link>
-          <Link href='/' className='bg-[#20AC58] px-5 rounded-md py-2'>Sign In</Link>
+          <Link href='/account' className='bg-[#20AC58] px-5 rounded-md py-2'>Sign In</Link>
         </div>
       </div>
     </nav>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import DisplaySections from "@/components/DisplaySections";
 import FetchData from "@/components/FetchData";
 import Navbar from "@/components/Navbar";
@@ -5,11 +7,35 @@ import TestComponent from "@/components/TestComponent";
 import HomePage from "@/components/HomePage";
 
 export default function Home() {
+  const router = useRouter();
+
+  // Gets the sectionn from the URL and loads it into view - this is for scrolling to the proper section when on a different page
+  useEffect(() => {
+    const section = router.query.section;
+    if (section) {
+      const element = document.getElementById(section);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [router.query]);
+
   return (
     <main className="">
       <Navbar />
-      <div className="pt-[100px]">
+      
+      <div id="Home" className="h-[100vh] pt-[70px]">
         <HomePage />
+      </div>
+
+      {/* Make an About component and render in this div */}
+      <div id="About" className="h-[100vh] pt-[70px]">
+        <h2>About section</h2>
+      </div>
+      
+      {/* Make a Features component and render in this div */}
+      <div id="Features" className="h-[100vh] pt-[70px]">
+        <h2>Features section</h2>
       </div>
     </main>
   );
