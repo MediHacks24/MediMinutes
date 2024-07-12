1789e5e022eba223582a5b747baf6136c93f019d
Aiden Mitchell <aidenmitchell747@gmail.com>
2024-07-11 23:44:56 -0600

    Fixed some annoying firebase folder stuff

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,6 +13,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   apiKey: "AIzaSyBi5lKmCbsdzIEQ3YIc0Hn4z9juBE4DES0",
   authDomain: "medimodule.firebaseapp.com",
+  databaseURL: "https://medimodule-default-rtdb.firebaseio.com",
   projectId: "medimodule",
   storageBucket: "medimodule.appspot.com",
   messagingSenderId: "580485646721",
@@ -18,9 +22,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
+
+
 
 // Initialize Firestore
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { db };
+// export const auth = firebase.auth(); // Export Firebase Authentication instance
+export { db, app, auth }
